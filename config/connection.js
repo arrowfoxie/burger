076288e9aee045c1ef455a64ca8698c0f0870a27
Verhana <arrowfoxie@gmail.com>
2018-09-13 21:18:24 -0500

    connected jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,16 @@ var mysql = require('mysql');
 var connection;
 
 // database details
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { 
 connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'burgers_db'
     });
-
+}
 // connects to database
 connection.connect(function (err) {
     if (err) {
